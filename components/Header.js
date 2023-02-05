0fdd6314299b5e283745e77cb606bc316b5bcd02
AyatAlKhulaqi <89496383+AyatAlKhulaqi@users.noreply.github.com>
2023-02-05 19:23:51 +0800

    Header is done, now working on the footer

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,20 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+import styles from './Header.module.css'
+
+export default function Header() {
+  return (
+  <>
+     <header className={styles.header}>
+      <div>
+        <h1>Ayat Al-Khulaqi</h1>
+        <nav>
+          <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </>
+  )
 }
