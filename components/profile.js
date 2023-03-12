c3dd97c9ee7cee6f5269636da699d0e646ffd1fc
AyatAlKhulaqi <89496383+AyatAlKhulaqi@users.noreply.github.com>
2023-03-12 14:43:01 +0800

    ...

--- components/profile.js
+++ components/profile.js
@@ -26,7 +26,7 @@ export default function Profile() {
         </div>
 
         <div className={styles.frame_img}>
-            <img src="../static/image/Frame.png"/>
+            <img src="/Frame.png"/>
         </div>
 
     </div>
